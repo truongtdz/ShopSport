7a626b2a4ec602e3ae148cfca0d61092c22e5c32
truongtdz <truongtd0910@gmail.com>
2024-12-25 15:26:51 +0700

    Completed project

--- SportShop/src/main/java/com/sportshop/sportshop/SportShopApplication.java
+++ SportShop/src/main/java/com/sportshop/sportshop/SportShopApplication.java
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SportShopApplication {
     public static void main(String[] args) {
         SpringApplication.run(SportShopApplication.class, args);
-    
     }
 }
